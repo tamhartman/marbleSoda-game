23e3890a93e073776feaaa453bca65d43e33a6f1
T-Rex <tamhartman@gmail.com>
2018-10-02 23:37:29 -0400

    general fixing of things. Added ball creation mechanism using space bar and water volume

---
+++ Marble Soda/Assets/timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour {
+
+    public GameObject ball;
+    private int numBalls = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space)){
+            Instantiate(ball);
+            numBalls = 1;
+        }
+       // if (numBalls > 0)
+	}
+}
