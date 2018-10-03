fad1863f9c70ed3a1ed6bd16ca41857fb55337b1
T-Rex <tamhartman@gmail.com>
2018-10-02 20:27:33 -0400

    fixed rotation issue

--- Marble Soda/Assets/Scripts/platformRotation.cs
+++ Marble Soda/Assets/Scripts/platformRotation.cs
@@ -11,21 +11,22 @@ public class platformRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.Rotate(75 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -75 * Time.deltaTime, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.Rotate(-75 * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0, 0, -75 * Time.deltaTime);
-        }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0,0, 75 * Time.deltaTime);
+            transform.Rotate(0, 75 * Time.deltaTime, 0);
         }
     }
 }
